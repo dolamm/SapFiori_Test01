a745405217100460f24a45d750d64738ddaaed53
Đỗ Công Lâm <lamdo@MacBook-Pro-cua-Lam.local>
2025-03-13 15:31:24 +0700

    operator filters

--- project1/webapp/controller/View1.controller.js
+++ project1/webapp/controller/View1.controller.js
@@ -6,9 +6,12 @@ sap.ui.define([
     "sap/m/Text",
     "sap/ui/table/Table",
     "sap/ui/table/Column",
+    "sap/ui/core/Item",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-], (Controller, ValueHelpDialog, JSONModel, Label, Text, Table, Column, Filter, FilterOperator) => {
+    "sap/ui/model/FilterOperator",
+    "sap/ui/comp/filterbar/FilterBar",
+    "sap/ui/comp/filterbar/FilterGroupItem"
+], (Controller, ValueHelpDialog, JSONModel, Label, Text, Table, Column, Item, Filter, FilterOperator, FilterBar, FilterGroupItem) => {
     "use strict";
 
     return Controller.extend("project1.controller.View1", {
@@ -42,32 +45,66 @@ sap.ui.define([
         onValueHelpRequestFilter: function (oEvent) {
             var oView = this.getView();
             var oInput = oEvent.getSource();
-
+            var sInputId = oInput.getId(); 
+            var that = this;
+        
             if (!this._oValueHelpDialog) {
                 this._oValueHelpDialog = new ValueHelpDialog({
-                    title: "Select Company Code",
+                    title: "Select Value",
                     supportMultiselect: true,
                     key: "Belnr",
+                    descriptionKey: "Bukrs",
                     ok: function (oEvent) {
                         var aTokens = oEvent.getParameter("tokens");
-
+                        var sOperator = that._oOperatorComboBox.getSelectedKey();
+                        
                         aTokens.forEach(token => {
                             var sKey = token.getKey(); 
                             token.setKey(sKey);
                             token.setText(sKey);
+                            token.data("operator", sOperator); 
                         });
-
+        
                         oInput.setTokens(aTokens);
-                        console.log(oInput.getTokens());
                         this.close();
                     },
                     cancel: function () {
                         this.close();
                     }
                 });
-
+        
                 oView.addDependent(this._oValueHelpDialog);
-
+        
+                this._oOperatorComboBox = new sap.m.ComboBox({
+                    width: "150px",
+                    placeholder: "Select Operator",
+                    items: [
+                        new Item({ key: "EQ", text: "Equals (=)" }),
+                        new Item({ key: "Contains", text: "Contains" }),
+                        new Item({ key: "StartsWith", text: "Starts With" }),
+                        new Item({ key: "EndsWith", text: "Ends With" }),
+                        new Item({ key: "NE", text: "Not Equal (≠)" }),
+                        new Item({ key: "GT", text: "Greater Than (>)" }),
+                        new Item({ key: "LT", text: "Less Than (<)" })
+                    ],
+                    selectedKey: "EQ"
+                });
+        
+                var oFilterBar = new FilterBar({
+                    advancedMode: true,
+                    filterBarExpanded: true,
+                    filterGroupItems: [
+                        new FilterGroupItem({
+                            groupName: "default",
+                            name: "operator",
+                            label: "Operator",
+                            control: this._oOperatorComboBox
+                        })
+                    ]
+                });
+                
+                this._oValueHelpDialog.setFilterBar(oFilterBar);
+        
                 // Create a table for the ValueHelpDialog
                 var oTable = new Table({
                     columns: [
@@ -86,21 +123,31 @@ sap.ui.define([
                         new Column({
                             label: new Label({ text: "Item" }),
                             template: new Text({ text: "{Buzid}" })
-                        }),
+                        })
                     ]
                 });
-
+        
                 this._oValueHelpDialog.setTable(oTable);
             }
+            
+            if (sInputId.indexOf("CompanyCodeFilter") !== -1) {
+                this._oValueHelpDialog.setTitle("Select Company Code");
+            } else if (sInputId.indexOf("Doc.NoFilter") !== -1) {
+                this._oValueHelpDialog.setTitle("Select Document Number");
+            }
+            
             //Set data for search help
             this._oValueHelpDialog.getTable().setModel(this._oSearchModel);
             this._oValueHelpDialog.getTable().bindRows("/");
+            
             // Open the ValueHelpDialog
             this._oValueHelpDialog.open();
         },
+        
         onBeforeRebindTable: function(oEvent) {
             var oBindingParams = oEvent.getParameter("bindingParams");
             var oDocNoFilter = this.byId("Doc.NoFilter");
+            var oCompanyCodeFilter = this.byId("CompanyCodeFilter");
             
             if (oDocNoFilter) {
                 var aDocNoTokens = oDocNoFilter.getTokens();
@@ -108,12 +155,54 @@ sap.ui.define([
                 if (aDocNoTokens && aDocNoTokens.length > 0) {
                     aDocNoTokens.forEach(function(oToken) {
                         var sValue = oToken.getKey();
-                        oBindingParams.filters.push(new Filter("Belnr", FilterOperator.EQ, sValue));
-                    });
+                        var sOperator = oToken.data("operator") || "EQ"; 
+                        var oFilterOp = this._getFilterOperator(sOperator);
+                        
+                        oBindingParams.filters.push(new Filter("Belnr", oFilterOp, sValue));
+                    }.bind(this));
+                }
+            }
+            
+            if (oCompanyCodeFilter) {
+                var aCompanyTokens = oCompanyCodeFilter.getTokens();
+                
+                if (aCompanyTokens && aCompanyTokens.length > 0) {
+                    aCompanyTokens.forEach(function(oToken) {
+                        var sValue = oToken.getKey();
+                        var sOperator = oToken.data("operator") || "EQ";
+                        var oFilterOp = this._getFilterOperator(sOperator);
+                        
+                        oBindingParams.filters.push(new Filter("Bukrs", oFilterOp, sValue));
+                    }.bind(this));
                 }
             }
             
             console.log("Filters:", oBindingParams.filters);
+        },
+        
+        _getFilterOperator: function(sOperator) {
+            switch(sOperator) {
+                case "EQ":
+                    return FilterOperator.EQ;
+                case "NE":
+                    return FilterOperator.NE;
+                case "GT":
+                    return FilterOperator.GT;
+                case "GE":
+                    return FilterOperator.GE;
+                case "LT":
+                    return FilterOperator.LT;
+                case "LE":
+                    return FilterOperator.LE;
+                case "Contains":
+                    return FilterOperator.Contains;
+                case "StartsWith":
+                    return FilterOperator.StartsWith;
+                case "EndsWith":
+                    return FilterOperator.EndsWith;
+                default:
+                    return FilterOperator.EQ;
+            }
         }
     });
 });
